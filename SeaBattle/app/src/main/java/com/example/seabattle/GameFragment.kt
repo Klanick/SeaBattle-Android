412ca6c7ca38f95130905bcdf8bd5f77075d6355
ayubjonumarov <jskha0666@gmail.com>
2023-04-03 05:01:24 +0300

    ayubjon_game: first step

--- SeaBattle/app/src/main/java/com/example/seabattle/GameFragment.kt
+++ SeaBattle/app/src/main/java/com/example/seabattle/GameFragment.kt
@@ -20,8 +20,26 @@ class GameFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentGameBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val gameButton = binding.quitButton
+
+        gameButton.setOnClickListener {
+            val fragment = MenuFragment()
+            val fragmentManager = requireActivity().supportFragmentManager
+            fragmentManager.popBackStack()
+            fragmentManager.beginTransaction()
+                .setReorderingAllowed(true)
+                .hide(fragmentManager.findFragmentByTag("menuFragment")!!)
+                .hide(fragmentManager.findFragmentByTag("gameFragment")!!)
+                .add(R.id.container, fragment, "menuFragment")
+                .addToBackStack(null)
+                .commit()
+        }
     }
 }
