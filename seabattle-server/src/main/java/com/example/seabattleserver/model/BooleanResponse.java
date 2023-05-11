75310a371ea632a397de51aafa6dc429a9a41df1
dark59146 <vova.sitnikov.1990@mail.ru>
2023-05-12 00:03:56 +0500

    Connected back to app

--- seabattle-server/src/main/java/com/example/seabattleserver/model/BooleanResponse.java
+++ seabattle-server/src/main/java/com/example/seabattleserver/model/BooleanResponse.java
@@ -2,9 +2,13 @@ package com.example.seabattleserver.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record BooleanResponse(@JsonProperty boolean response) {
+public record BooleanResponse(@JsonProperty boolean response, @JsonProperty String errorMessage) {
 
     public static BooleanResponse of(boolean value) {
-        return new BooleanResponse(value);
+        return new BooleanResponse(value, null);
+    }
+
+    public static BooleanResponse error(String message) {
+        return new BooleanResponse(false, message);
     }
 }
